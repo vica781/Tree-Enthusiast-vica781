fdb6a777c24638299cfef2fd414a8e719d83da38
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-01-05 18:00:41 +0000

    Create first view and url

--- tree_enthusiast/settings.py
+++ tree_enthusiast/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6*ylap*(c3hp7&r95-a2nb(4jk==+yqd51u5wow5u)$3#x%toy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'Victoria-Laptop', 'DESKTOP-5QJH8QH']
 
 # Application definition
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tree_identification',
 ]
 
 MIDDLEWARE = [
