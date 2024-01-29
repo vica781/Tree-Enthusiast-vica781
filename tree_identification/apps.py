32da6210c802b06da59fe528e5d443b1e724828a
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-01-29 18:26:12 +0000

    Wip: add signals.py

--- tree_identification/apps.py
+++ tree_identification/apps.py
@@ -2,5 +2,12 @@ from django.apps import AppConfig
 
 
 class TreeIdentificationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tree_identification'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tree_identification"
+
+
+class TreeIdentificationConfig(AppConfig):
+    name = "tree_identification"
+
+    def ready(self):
+        import tree_identification.signals
