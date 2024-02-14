9d5572788ad84fb6b770b53786fab9e189e85449
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-02-14 13:57:27 +0000

    Feat (wip): add back-to-top btn to 'My trees'

--- tree_identification/static/assets/js/script.js
+++ tree_identification/static/assets/js/script.js
@@ -32,5 +32,23 @@ document.addEventListener("DOMContentLoaded", function () {
       passwordInput.type = "password";
       toggleButton.textContent = "Show Password";
     }
+}
+
+// My Trees viewing back-to-top button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
   }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+
   
