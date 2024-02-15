2faa50caad5ef359e900c03f5dc360764bb28110
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-02-15 12:59:00 +0000

    Style: change toggleTreePasswordButton style

--- tree_identification/static/assets/js/script.js
+++ tree_identification/static/assets/js/script.js
@@ -55,13 +55,13 @@ function topFunction() {
 
 // Password conformation and view/hide password (tree_detail.html)
 function toggleTreePassword() {
-  let passwordInput = document.getElementById("tree-confirm-password");
-  let toggleButton = document.getElementById("toggleTreePasswordButton");
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-    toggleButton.textContent = "Hide Password";
-  } else {
-    passwordInput.type = "password";
-    toggleButton.textContent = "Show Password";
-  }
-}
+        let passwordInput = document.getElementById("tree-confirm-password");
+        let toggleButton = document.getElementById("toggleTreePasswordButton");
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            toggleButton.textContent = "Hide Password";
+        } else {
+            passwordInput.type = "password";
+            toggleButton.textContent = "Show Password";
+        }
+    }
