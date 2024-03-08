1a0518e49b3d28e44416b655c6fc3cb0641145e7
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-03-08 18:11:06 +0000

    Deployment commit

--- tree_enthusiast/settings.py
+++ tree_enthusiast/settings.py
@@ -32,7 +32,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "tree_identification/templates")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = "SAMEORIGIN"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
