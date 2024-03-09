fc4e76dd3509fa2844e4520f093f5a981e09f409
Victoria Kalenik-Harris <vk781@hotmail.com>
2024-03-09 19:29:25 +0000

    Fix: reinstate accidentally deleted search_results.html

--- tree_enthusiast/settings.py
+++ tree_enthusiast/settings.py
@@ -41,7 +41,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "tree_identification/templates")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
